8b1afa42d1ee68843bfd23d5386eb7ebade13b90
JagadeeshReddy657 <jagadeshreddyyenugula@gmail.com>
2026-06-03 13:45:45 +0530

    Hyperparameter Tuning

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -7,6 +7,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder,StandardScaler
+#from streamlit import columns
 
 from src.exception import CustomException
 from src.logger import logging
@@ -88,10 +89,10 @@ class DataTransformation:
             target_column_name="math_score"
             numerical_columns = ["writing_score", "reading_score"]
 
-            input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
+            input_feature_train_df=train_df.drop(columns=[target_column_name])
             target_feature_train_df=train_df[target_column_name]
 
-            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
+            input_feature_test_df=test_df.drop(columns=[target_column_name])
             target_feature_test_df=test_df[target_column_name]
 
             logging.info(
